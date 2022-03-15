5334a88fbee6078ce7d16b3f9fb58da517f42d86
Michael Saunby <mike@saunby.net>
2022-03-15 17:19:20 +0000

    demo

--- code/tests/test_musicdb.py
+++ code/tests/test_musicdb.py
@@ -12,5 +12,14 @@ import musicdb
 def test_MusicDB_get_album_names(connect: MagicMock):
     db = musicdb.MusicDB("file://BAD")
     db.get_album_names()
-    print(connect.mock_calls)
-    assert True
+    # Checking if a method of a method... etc. has been called is possible,
+    # but it requires some knowledge of what methods have been called.
+    ## You can get a list by uncommenting the next two lines.
+    # print(connect.mock_calls)
+    # assert False
+    ## The line to test for is
+    ## 'call().__enter__().cursor().execute('SELECT DISTINCT album FROM tunes'
+    ## Where thing() appears this translates to a mock of return_value.thing 
+
+    cursor_mock: MagicMock = connect.return_value.__enter__.return_value.cursor.return_value
+    cursor_mock.execute.assert_called_with("SELECT DISTINCT album FROM tunes")
